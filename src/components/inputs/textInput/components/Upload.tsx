07a9525037edd9f9185434bd950998186a080a44
niztal <nitzan.tal2@gmail.com>
2024-01-04 14:28:35 +0200

    linting

--- src/components/inputs/textInput/components/Upload.tsx
+++ src/components/inputs/textInput/components/Upload.tsx
@@ -1,9 +1,7 @@
-
 export const ProgressCircle = () => {
   document.addEventListener('click', doSomething, false);
 
   function doSomething(e: any) {
-
     const chatbot = document.querySelector('flowise-chatbot');
     if (!!chatbot && !!chatbot.shadowRoot) {
       const percentage = Math.floor(Math.random() * 10);
@@ -24,9 +22,36 @@ export const ProgressCircle = () => {
   }
 
   return (
-    <svg id='progressBarContainer' width='50' viewBox='-14.125 -14.125 141.25 141.25' version='1.1' xmlns='http://www.w3.org/2000/svg' style='transform:rotate(-90deg)'>
-      <circle r='46.5' cx='56.5' cy='56.5' fill='transparent' stroke='#e0e0e0' stroke-width='13' stroke-dasharray='292.02000000000004px' stroke-dashoffset='0'></circle>
-      <circle id='progress-circle' r='46.5' cx='56.5' cy='56.5' stroke='#76e5b1' stroke-width='9' stroke-linecap='round' stroke-dashoffset='219px' fill='transparent' stroke-dasharray='292.02000000000004px'></circle>
+    <svg
+      id="progressBarContainer"
+      width="50"
+      viewBox="-14.125 -14.125 141.25 141.25"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      style={{ transform: 'rotate(-90deg)' }}
+    >
+      <circle
+        r="46.5"
+        cx="56.5"
+        cy="56.5"
+        fill="transparent"
+        stroke="#e0e0e0"
+        stroke-width="13"
+        stroke-dasharray="292.02000000000004px"
+        stroke-dashoffset="0"
+      />
+      <circle
+        id="progress-circle"
+        r="46.5"
+        cx="56.5"
+        cy="56.5"
+        stroke="#76e5b1"
+        stroke-width="9"
+        stroke-linecap="round"
+        stroke-dashoffset="219px"
+        fill="transparent"
+        stroke-dasharray="292.02000000000004px"
+      />
     </svg>
   );
-}
+};
