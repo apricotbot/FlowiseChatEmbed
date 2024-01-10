357adb2b23dbaa694f42822a940b8959e7bd15dc
niztal <nitzan.tal2@gmail.com>
2024-01-10 10:17:55 +0200

    commit dist files

--- dist/components/inputs/textInput/components/Upload.d.ts
+++ dist/components/inputs/textInput/components/Upload.d.ts
@@ -3,12 +3,15 @@ type UploadProps = {
     customerId?: string;
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     uploadColor?: string;
 =======
 >>>>>>> a72bf23 (commit dist files)
 =======
     uploadColor?: string;
 >>>>>>> 3ac3312 (commit dist)
+=======
+>>>>>>> e0cdf25 (commit dist files)
     onUpload: (isSuccess: boolean, message: string) => void;
 };
 export declare const Upload: (props: UploadProps) => import("solid-js").JSX.Element;
