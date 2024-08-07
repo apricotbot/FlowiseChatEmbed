61d13cff453a1e6aff456f032e258f834cabf86e
niztal <nitzan.tal2@gmail.com>
2024-08-07 18:07:52 +0300

    support RTL

--- src/features/bubble/components/Bubble.tsx
+++ src/features/bubble/components/Bubble.tsx
@@ -95,6 +95,7 @@ export const Bubble = (props: BubbleProps) => {
           'z-index': 42424242,
           bottom: `${Math.min(buttonPosition().bottom + buttonSize + 10, window.innerHeight - chatWindowBottom)}px`,
           right: `${Math.min(buttonPosition().right, window.innerWidth - 410)}px`,
+          direction: props.theme?.chatWindow?.direction === 'rtl' ? 'rtl' : 'ltr',
         }}
         class={
           `fixed sm:right-5 rounded-lg w-full sm:w-[400px] max-h-[704px]` +
